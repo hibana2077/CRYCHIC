cf05a5455165fbfab8465cb7bae548a382aff36d
hibana2077 <hibana2077@gmail.com>
2025-01-07 19:37:17 +0800

    Add installation script for pyskl with conda environment setup

---
+++ main/install_pyskl.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/kennymckormick/pyskl.git
+cd pyskl
+# This command runs well with conda 22.9.0, if you are running an early conda version and got some errors, try to update your conda first
+conda env create -f pyskl.yaml
+conda activate pyskl
+pip install -e .
