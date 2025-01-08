0fd84304ccefe37ac80e4c7f0ec5d9f0688dd8e9
hibana2077 <hibana2077@gmail.com>
2025-01-08 08:56:04 +0800

    Add download commands for additional datasets in download_skl.sh

--- main/download_skl.sh
+++ main/download_skl.sh
@@ -3,3 +3,20 @@ mkdir -p data/gym
 cd data/gym
 echo "$(pwd)"
 wget https://download.openmmlab.com/mmaction/pyskl/data/gym/gym_hrnet.pkl
+cd ..
+mkdir -p ucf101
+cd ucf101
+echo "$(pwd)"
+wget https://download.openmmlab.com/mmaction/pyskl/data/ucf101/ucf101_hrnet.pkl
+cd ..
+mkdir -p hmdb51
+cd hmdb51
+echo "$(pwd)"
+wget https://download.openmmlab.com/mmaction/pyskl/data/hmdb51/hmdb51_hrnet.pkl
+cd ..
+mkdir -p diving48
+cd diving48
+echo "$(pwd)"
+wget https://download.openmmlab.com/mmaction/pyskl/data/diving48/diving48_hrnet.pkl
+
+echo "Downloaded all the files"
