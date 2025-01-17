77ea53e654c397b46773ed0fec25c41e936233e7
hibana2077 <hibana2077@gmail.com>
2025-01-17 17:28:02 +0800

    Refactor download_skl.sh to clean up commented-out code and improve readability

--- main/download_skl.sh
+++ main/download_skl.sh
@@ -1,18 +1,22 @@
 cd pyskl
+
 mkdir -p data/gym
 cd data/gym
 echo "$(pwd)"
 wget https://download.openmmlab.com/mmaction/pyskl/data/gym/gym_hrnet.pkl
+
 # cd ..
 # mkdir -p ucf101
 # cd ucf101
 # echo "$(pwd)"
 # wget https://download.openmmlab.com/mmaction/pyskl/data/ucf101/ucf101_hrnet.pkl
+
 # cd ..
 # mkdir -p hmdb51
 # cd hmdb51
 # echo "$(pwd)"
 # wget https://download.openmmlab.com/mmaction/pyskl/data/hmdb51/hmdb51_hrnet.pkl
+
 # cd ..
 # mkdir -p diving48
 # cd diving48
