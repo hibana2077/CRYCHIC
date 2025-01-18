cfdb5e481336838104f5b6bbb1572d38f629ac6d
hibana2077 <hibana2077@gmail.com>
2025-01-18 13:22:21 +0800

    Update work directory paths in joint.py and limb.py for CryChic model consistency; modify installation script comments for clarity

--- main/install_pyskl.sh
+++ main/install_pyskl.sh
@@ -1,9 +1,11 @@
 git clone https://github.com/kennymckormick/pyskl.git
+mv pyskl_patch/models/cnns/* pyskl/pyskl/models/cnns/
+mv configs/* pyskl/configs/
 cd pyskl
 # This command runs well with conda 22.9.0, if you are running an early conda version and got some errors, try to update your conda first
 conda env create -f pyskl.yaml
 conda activate pyskl
 pip install -e .
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. "$HOME/.cargo/env"
-pip install timm #  OMG it can work with python 3.7 -> only support to 0.9.12
+# . "$HOME/.cargo/env" # still manually run this command
+# pip install timm #  OMG it can work with python 3.7 -> only support to 0.9.12
